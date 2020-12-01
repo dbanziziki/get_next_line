de90db697d770c7a948c6730e2d95875052464b7
David <alexdavid2@live.fr>
2020-12-01 12:09:38 +0100

    multi buffers

--- main.c
+++ main.c
@@ -6,19 +6,17 @@
 int main(int ac, char **av)
 {
 	char *line;
-	char *line2;
 	int fd1;
-	int fd2;
+	int i;
 	(void)ac;
+
+	i = 0;
 	fd1 = open(av[1], O_RDONLY);
-	fd2 = open(av[2], O_RDONLY);
-	
-	while (get_next_line(fd1, &line) == 1 && get_next_line(fd2, &line2) == 1)
+	while (get_next_line(fd1, &line) == 1)
 	{
-		printf("[fd1: ]%s\n", line);
-		printf("[fd2: ]%s\n", line2);
+		i++;
+		printf("[%d] %s\n", i, line);
 		free(line);
-		free(line2);
 	}
 	return (0);
 }
