04f30175f9a4e25deb178a457618ebb9dbbe5fac
David Banziziki <dbanzizi@mi-r1-p3.s19.be>
2021-03-24 15:33:37 +0100

    protection added

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -3,25 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbanzizi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dbanzizi <dbanzizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 12:35:40 by dbanzizi          #+#    #+#             */
-/*   Updated: 2020/12/15 23:08:16 by dbanzizi         ###   ########.fr       */
+/*   Updated: 2021/03/24 15:21:29 by dbanzizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static int	save_buffer(char **file, char *buffer, int ret)
 {
 	char	*temp_content;
 
 	buffer[ret] = '\0';
+	temp_content = 0;
 	if (*file == NULL)
-		temp_content = ft_strndup(buffer, ret);
+	{
+		if (!(temp_content = ft_strndup(buffer, ret)))
+			return (-1);
+	}
 	else
 	{
-		temp_content = ft_strjoin(*file, buffer);
+		if (!(temp_content = ft_strjoin(*file, buffer)))
+			return (-1);
 		free(*file);
 	}
 	*file = temp_content;
@@ -30,21 +35,30 @@ static int	save_buffer(char **file, char *buffer, int ret)
 	return (0);
 }
 
-static int	extract_line(char **src, char **line)
+static int	ft_cpy_line(char **src, char **line, int len)
 {
 	char	*temp1;
+
+	if (!(*line = ft_strndup(*src, len)))
+		return (-1);
+	if (!(temp1 = ft_strndup(*src + (len + 1), ft_strlen(*src + (len + 1)))))
+		return (-1);
+	free(*src);
+	*src = temp1;
+	return (1);
+}
+
+static int	extract_line(char **src, char **line)
+{
 	size_t	len;
 
+	if (!src || !(*src))
+		return (0);
 	len = 0;
 	while ((*src)[len] && (*src)[len] != '\n')
 		len++;
 	if ((*src)[len] == '\n')
-	{
-		*line = ft_strndup(*src, len);
-		temp1 = ft_strndup(*src + (len + 1), ft_strlen(*src + (len + 1)));
-		free(*src);
-		*src = temp1;
-	}
+		return (ft_cpy_line(src, line, len));
 	else
 	{
 		*line = *src;
@@ -72,5 +86,7 @@ int			get_next_line(int fd, char **line)
 			break ;
 	}
 	free(buffer);
-	return (ret < 0 ? -1 : extract_line(&file[fd], line));
+	if (ret < 0)
+		return (-1);
+	return (extract_line(&file[fd], line));
 }
